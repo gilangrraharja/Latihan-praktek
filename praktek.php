be6fbcc9a311ff29246c62bed7d151939116ba36
R Gilang Rizkia Raharja <gilangrizkia331@gmail.com>
2017-02-02 11:12:32 +0700

    Add files via upload

---
+++ praktek.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+ <title>Ini Contoh 2</title>
+ <body>
+    <?php
+      //ini komentar yang bisa satu baris
+      /* ini komentar yang
+         bisa 2 baris
+         atau lebih
+      */
+    ?>
+ </body>
+</html>
